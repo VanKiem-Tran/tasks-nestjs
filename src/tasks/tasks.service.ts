a6bf7b20c6d19987058bc29a96f99047df2a6657
naV-meiK <87219641+naV-meiK@users.noreply.github.com>
2021-12-06 15:48:25 +0700

    upload

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,46 @@
+import {Injectable, NotFoundException} from '@nestjs/common'
+import {InjectRepository} from '@nestjs/typeorm'
+import {User} from 'src/auth/user.entity'
+import {CreateTaskDto} from './dto/create-task.dto'
+import {GetTasksFilterDto} from './dto/get-tasks-filter.dto'
+import {TaskStatus} from './task-status.enum'
+import {Task} from './task.entity'
+import {TaskRepository} from './task.repository'
+
+@Injectable()
+export class TasksService {
+  constructor(
+    @InjectRepository(TaskRepository)
+    private taskRepository: TaskRepository
+  ) {}
+
+  async getTask(filterDto: GetTasksFilterDto, user: User) {
+    return this.taskRepository.getTask(filterDto, user)
+  }
+
+  async getTaskById(id: number): Promise<Task> {
+    const found = await this.taskRepository.findOne(id)
+    if (!found) {
+      throw new NotFoundException(`Task with ID "${id}" not found`)
+    }
+    return found
+  }
+
+  async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
+    return this.taskRepository.createTask(createTaskDto, user)
+  }
+
+  async deleteTask(id: number): Promise<void> {
+    const result = await this.taskRepository.delete(id)
+    if (result.affected === 0) {
+      throw new NotFoundException(`Task with ID "${id}" not found`)
+    }
+  }
+
+  async updateTask(id: number, status: TaskStatus): Promise<Task> {
+    const task = await this.getTaskById(id)
+    task.status = status
+    await task.save()
+    return task
+  }
+}
